7a5817b0748f7fef561948199669b74895e0692d
jenny <jenny88062@gmail.com>
2022-05-12 19:17:13 +0800

    count line len avg

--- review_analytics.py
+++ review_analytics.py
@@ -12,9 +12,13 @@ with open('reviews.txt', 'r') as f:
       data.append(line) 
       count = count + 1
       if count % 1000 ==0:
-          print(len(data))
-        
-print(len(data))
-print(data[0])
+          print(len(data))       
+print('檔案讀取完成,共有', len(data), '筆資料')
 
+sumlen = 0
+for LongLine in data:
+    sumlen = sumlen +len(LongLine)
+
+print('留言的平均長度是', sumlen/len(data))
+     
 
